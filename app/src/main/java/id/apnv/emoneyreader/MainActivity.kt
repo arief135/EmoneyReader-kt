25c5669a253089c0b6bf0697886ac44ebc49933f
Arief Pratama <ariefpratama135@gmail.com>
2024-01-08 22:05:00 +0700

    detect NFC

--- app/src/main/java/id/apnv/emoneyreader/MainActivity.kt
+++ app/src/main/java/id/apnv/emoneyreader/MainActivity.kt
@@ -1,13 +1,23 @@
 package id.apnv.emoneyreader
 
+import android.content.Context
+import android.nfc.NfcManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import id.apnv.emoneyreader.ui.theme.EmoneyReaderTheme
@@ -22,7 +32,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val nfcManager = this.getSystemService(Context.NFC_SERVICE) as NfcManager
+                    NfcTool(nfcManager)
                 }
             }
         }
@@ -30,17 +41,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun NfcTool(nfcManager: NfcManager?, modifier: Modifier = Modifier) {
+    var status by remember { mutableStateOf("IDLE") }
+    var nfcReady by remember { mutableStateOf("NFC not ready") }
+
+    if (nfcManager != null && nfcManager.defaultAdapter.isEnabled) {
+        nfcReady = "NFC is ready"
+    }
+
+    val onClickHandler = {
+        status = "WAIT"
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
     )
+    {
+        Text(
+            text = nfcReady,
+            modifier = modifier
+        )
+        Button(
+            onClick = onClickHandler
+        ) {
+            Text(text = "Read Tag")
+        }
+        Text(
+            text = "Status: $status",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     EmoneyReaderTheme {
-        Greeting("Android")
+        NfcTool(null)
     }
 }
